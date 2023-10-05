b5c01791cbb385ded78ca1e4a84e573720827d64
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-10-05 20:32:01 +0300

    Add files via upload

---
+++ 4xZnachnoe(chislo).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    setlocale(LC_ALL, "rus");
+    cout << "Введите 4-х значное число\n";
+    cin >> a;
+    if (a / 1000 == 0 || a / 10000 > 0) {
+        cout << "Ошибка";
+    }
+    else {
+        cout << (a / 1000) * 100 + ((a / 100) % 10) * 1000 + ((a / 10) % 10) + (a % 10) * 10;
+    }
+}
+
---
+++ maximal chislo(cemiznach).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a[7], b;
+    cout << "Введите 7 целых чисел \n";
+    cin >> a[0] >> a[1] >> a[2] >> a[3] >> a[4] >> a[5] >> a[6];
+    for (int c{ 1 }; c < 7; c++) {
+        b = a[0];
+        if (a[c] > b) {
+            b = a[c];
+        }
+    }
+    cout << "Наибольшее число из введенных " << b;
+}
+
