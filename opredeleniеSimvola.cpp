f114157074c239d7a81a806e39ca48d54859abf7
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-10-05 20:40:47 +0300

    Add files via upload

---
+++ opredeleniеSimvola.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char a;
+    cout << "Введите любой символ ";
+    cin >> a;
+    if (a >= 'a' && a <= 'z') {
+        cout << "Вы ввели букву";
+    }
+    else if (a >= '0' && a <= '9') {
+        cout << "Вы ввели цифру";
+    }
+    else if (a >= '!' && a <= '.') {
+        cout << "Вы ввели знак препинания";
+    }
+    else {
+        cout << "Неизвестный символ";
+    }
+}
+
