a1f085fa61d41be81fd2ef628b2f9d5e1364be0e
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-11-11 21:07:51 +0300

    Add files via upload

---
+++ prostoeChislo.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a;
+	cout << "Введите число ";
+	cin >> a;
+	bool flag = true;
+	for (int i = 2; i <= sqrt(a); i++) {
+		if (a % i == 0) {
+			flag = false;
+			break;
+		}
+	}
+	cout << endl << ((flag) ? "Да, число простое " : "Нет, не простое ");
+}
