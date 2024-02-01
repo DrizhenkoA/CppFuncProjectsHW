93e589166b1f306cc7462424e02a6fcdb878ce6a
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2024-02-01 13:11:59 +0300

    Add files via upload

---
+++ lesson_01_02_2024.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+//template <typename T>
+//                        //бинарный поиск в динамическом массиве;
+//int binarySearchRecursive(int* arr, int target, int left, int right) {
+// if (left > right) return -1;
+//
+// int mid = left + (right - left) / 2;
+// cout << "Вызвали " << mid << endl;
+// if (arr[mid] == target) {
+//  return mid;
+// }
+// else if (arr[mid] > target) {
+//  return binarySearchRecursive(arr, target, left,mid-1);
+// }
+// else {
+//  return binarySearchRecursive(arr, target, mid + 1,right);
+// }
+//}
+//
+//int binarySearch(int* arr, int target) {
+// int size = _msize(arr) / sizeof(arr[0]);
+// return binarySearchRecursive(arr, target, 0, size - 1);
+//}
+//
+//int main()
+//{
+// setlocale(LC_ALL, "rus");
+// srand(time(NULL));
+// int* array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+// int target = 8;
+// cout << binarySearch(array, target);
+//
+//}
+
+
+                        //нахождения факториала в функции типа void;
+
+void fact(int num, int result = 1) {
+    if (num == 1) {
+        cout << result;
+        return;
+    }
+    fact(num - 1, result * num);
+    if (num <= 0) {
+        cout << " ошибка " << endl;
+        return;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    int num = 5;
+    cout << "факториал " << num << " = ";
+    fact(num);
+
+}
