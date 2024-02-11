e7d009ebd449a51f4ab00c7e7a0ea5a2ae03e007
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2024-02-11 18:50:28 +0300

    Add files via upload

---
+++ dz_10_02_2024.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");                      //bubble_sort
+    srand(time(NULL));
+    const int SIZE = 10;
+    int arr[SIZE] = {};
+    cout << "до сортировки" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = 1 + rand() % 20;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE-1; j++) {
+            if (arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    }
+
+    cout << "после сортировки" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
